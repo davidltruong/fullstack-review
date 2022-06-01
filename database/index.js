caa3a4b0e6ee55fa3a2be85754d5cc6f6604aa50
David Truong <dtruong1992@gmail.com>
2022-06-01 00:21:01 -0700

    Complete save function for database

--- database/index.js
+++ database/index.js
@@ -3,15 +3,30 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
-  repoName: String,
-  username: String,
-  stargazers: Number,
-  url: String
+  name: String,
+  owner: String,
+  stargazers_count: Number,
+  git_url: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (repos) => {
+  for (var i = 0; i < repos.length; i++) {
+    var test = Repo.find({name: repos[i].name}, function(err, arr) {
+      if (err) {
+        var repo = new Repo({
+          name: repos[i].name,
+          owner: repos[i].owner.login,
+          stargazers_count: repos[i].stargazers_count,
+          git_url: repos[i].html_url
+        })
+      } else if (arr) {
+        console.log('duplicate')
+      }
+    })
+
+  }
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
